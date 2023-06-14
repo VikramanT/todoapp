e913bcfdac827a5c3b86f929d2df267ccde42568
VikramanT <104017683+VikramanT@users.noreply.github.com>
2023-06-14 16:26:13 +0530

    Add existing file

--- lib/screens/newtask.dart
+++ lib/screens/newtask.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:crypto/crypto.dart';
-
-
 class TodoScreen extends StatefulWidget {
   @override
   _TodoScreenState createState() => _TodoScreenState();
@@ -15,42 +11,19 @@ class TodoScreen extends StatefulWidget {
 class _TodoScreenState extends State<TodoScreen> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  addtask() async{
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final User? user = auth.currentUser;
+  String? uid =user?.uid;
+  var time=DateTime.now();
+  await FirebaseFirestore.instance.collection('tasks').doc(uid).collection('mytask').doc(time.toString()).set({'title':titleController.text,
+  'description':descriptionController.text,
+  'time':time.toString(),
+  'timestamp':time
+  });
+  Fluttertoast.showToast(msg: 'message added');
 
-  String encryptData(String data, String secretKey) {
-    final key = utf8.encode(secretKey);
-    final bytes = utf8.encode(data);
-
-    final hmacSha256 = Hmac(sha256, key);
-    final digest = hmacSha256.convert(bytes);
-
-    final encryptedData = base64Url.encode(digest.bytes);
-    return encryptedData;
   }
-
-  addTask() async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    final User? user = auth.currentUser;
-    String? uid = user?.uid;
-    var time = DateTime.now();
-
-    String encryptedTitle = encryptData(titleController.text, "your-secret-key");
-    String encryptedDescription = encryptData(descriptionController.text, "your-secret-key");
-
-    await FirebaseFirestore.instance
-        .collection('tasks')
-        .doc(uid)
-        .collection('mytask')
-        .doc(time.toString())
-        .set({
-      'title': encryptedTitle,
-      'description': encryptedDescription,
-      'time': time.toString(),
-      'timestamp': time
-    });
-
-    Fluttertoast.showToast(msg: 'message added');
-  }
-
   @override
   void dispose() {
     // Dispose the controllers when the widget is disposed
@@ -62,7 +35,7 @@ class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff28284d),
+            backgroundColor: Color(0xff28284d),
       appBar: AppBar(
         title: Text('Todo App'),
         backgroundColor: Color(0xfffa7a40),
@@ -105,9 +78,17 @@ class _TodoScreenState extends State<TodoScreen> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xfffa7a40),
         onPressed: () {
-          addTask();
+          // Handle saving the todo with the entered title and description
+          // You can add your logic here
+          // String title = titleController.text;
+          // String description = descriptionController.text;
+          // print('Title: $title');
+          // print('Description: $description');
+        addtask();
+          // Clear the text fields after saving
           titleController.clear();
           descriptionController.clear();
+          
         },
         child: Icon(Icons.save),
       ),
